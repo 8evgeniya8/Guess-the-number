80b8af22e542dce0fda6b46684c08291bbb6a463
Tsukanova Evgeniya <tata@gmail.com>
2023-04-22 18:28:53 +0300

    1 commit

--- src/Tsukanova/Dz6Number/GuessNumber.java
+++ src/Tsukanova/Dz6Number/GuessNumber.java
@@ -5,40 +5,35 @@ import java.util.Scanner;
 public class GuessNumber {
     public static void main(String[] args) {
         Scanner sca = new Scanner(System.in);
-        System.out.println("Вгадай число від 0 до 10");
-        System.out.println("В тебе є 3 спроби");
+        System.out.println("Гра \"Вгадай число\" (^‿^). В тебе є три спроби");
         int a = 0;
-        int b = 4;
-
+        int c = 2;
+        int random = (int) (Math.random() * 11);
         while (true) {
-            a++;
-            if (a >= b) {
-                System.out.println("Гра закінчилась,спробуй ще");
-                break;
-            }
-            if (a < b) {
-                if (sca.hasNextInt()) {
-                    int random = (int) (Math.random() * 11);
-                    int userValue = sca.nextInt();
-                    if (userValue >= 0 && userValue <= 10) {
-                        System.out.println("Введене число = " + userValue);
-                        System.out.println("Moє задуманне число: " + random);
-                    }
+            if (sca.hasNextInt()) {
+                int userValue = sca.nextInt();
+                a++;
+                if (a <= c) {
                     if (userValue == random) {
-                        System.out.println("WIN!!! (^‿^)");
+                        System.out.println("WIN!!! ٩(｡•́‿•̀｡)۶");
+                        break;
                     } else if (userValue > random) {
-                        System.out.println("Введи число від 0 до 10");
+                        System.out.println("Введи число від 0 до 10 (◕‿◕)");
+                        continue;
                     } else if (userValue != random) {
-                        System.out.println("Програв (◕‿◕)");
+                        System.out.println("Ні (◕‿◕)");
                     }
                 } else {
-                    System.out.println("Помилка.Спробуй знову");
+                    System.out.println("Я загадав :" + random);
+                    System.out.println("Програв.Спробуй ще (◕‿◕)");
+                    break;
                 }
+            } else {
+                System.out.println("Введи лише цілі числа (◕‿◕)");
+                sca.nextLine();
             }
             sca.nextLine();
         }
-
-
     }
 }
 
